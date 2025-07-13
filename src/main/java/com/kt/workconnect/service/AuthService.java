d9ef0fd70d3ea596fe13967952752995a83eb8c7
Kavindu Thiwanka <214689990+Kavindu-Thiwanka@users.noreply.github.com>
2025-07-13 16:44:58 +0530

    remove old changes

--- src/main/java/com/kt/workconnect/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.kt.workconnect.service;
-
-import com.kt.workconnect.dto.RegisterRequestDTO;
-import org.springframework.http.ResponseEntity;
-
-public interface AuthService {
-
-    ResponseEntity<?> registerUser(RegisterRequestDTO registerRequestDTO);
-}
